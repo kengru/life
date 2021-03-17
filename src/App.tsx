373d763c3e8b99f6dae8053882801539f9184891
Kendry Grullon <kengrullon@gmail.com>
2021-03-17 09:40:34 -0400

    Initialized.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Table } from "./components/Table";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+
+export default App;
