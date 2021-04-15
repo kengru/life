661f5045e8c5e2a3d616fb31f7a77294624f9c9f
kengru <kengrullon@gmail.com>
2021-04-14 20:12:39 -0400

    Initializing tests creation.

---
+++ src/components/Life/Row.test.tsx
@@ -0,0 +1,13 @@
+// import { render } from "@testing-library/react";
+// import { Row } from "./Row";
+
+// const testingProps = {
+//   columns: 2,
+//   keyId: 1
+// };
+
+// describe("Row", () => {
+//   test("It renders a row of cells", () => {
+//     render(<Row {...testingProps} />);
+//   });
+// });
---
+++ src/components/UI/Button.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { Button } from "./Button";
+
+const testingProps: ButtonProps = {
+  type: "clear",
+  action: () => {}
+};
+
+describe("Button", () => {
+  test("It renders a Button", () => {
+    render(<Button {...testingProps} />);
+
+    expect(screen.getByText("Clear")).toBeInTheDocument();
+  });
+});
