1906556e659baaa52b1b4f7e5e75f0e789ed4188
Kendry Grullon <kengrullon@gmail.com>
2021-04-30 11:32:28 -0400

    Created the canvas for the elementary CA.

--- src/components/ElementaryCA/Canvas.tsx
+++ src/components/ElementaryCA/Canvas.tsx
@@ -1,17 +1,119 @@
-import { Fragment } from "react";
-import { Rules } from "./Rules";
-import { Button } from "../UI/Button";
+import Sketch from "react-p5";
+import p5Types from "p5";
 
-export const Canvas = () => (
-  <Fragment>
-    <div className="elementaryFront">
-      <p className="elementaryTitle">Ken's Cellular Automata 1D</p>
-      <p className="elementarySubtitle">Define a rule for each thing</p>
-    </div>
-    <Rules />
-    <p className="elementaryRuleNumber">Rule 30</p>
-    <div className="elementaryStart">
-      <Button type="start" action={() => {}} />
-    </div>
-  </Fragment>
-);
+const cells: Cell[][] = [];
+const rules: Record<string, boolean> = {
+  "0 0 0": false,
+  "0 0 1": true,
+  "0 1 0": true,
+  "0 1 1": true,
+  "1 0 0": true,
+  "1 0 1": false,
+  "1 1 0": false,
+  "1 1 1": false
+};
+const speed = 20;
+const size = 4;
+const lines = 90;
+const max = 1 + 2 * lines;
+
+export const Canvas = () => {
+  const defH = size * lines;
+  const defW = size * max;
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.createCanvas(defW, defH).parent(canvasParentRef);
+    const initialRow = [];
+    for (let i = 0; i < max; i++) {
+      if (i === Math.floor(max / 2)) {
+        initialRow.push(new Cell(i * size, 0, size, true, p5));
+      } else {
+        initialRow.push(new Cell(i * size, 0, size, false, p5));
+      }
+    }
+    cells.push(initialRow);
+    p5.frameRate(speed);
+  };
+
+  const draw = (p5: p5Types) => {
+    p5.background(220);
+    cells.forEach((row) => {
+      row.forEach((cell) => {
+        cell.show();
+      });
+    });
+    if (cells.length < lines) {
+      cells.push(createNewAr(cells[cells.length - 1], cells.length, p5));
+    }
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+};
+
+class Cell {
+  pos;
+  size;
+  color;
+  active;
+  p5;
+
+  constructor(
+    x: number,
+    y: number,
+    size: number,
+    active: boolean,
+    p5: p5Types
+  ) {
+    this.pos = p5.createVector(x, y);
+    this.size = size;
+    this.color = p5.color(240);
+    this.active = active;
+    this.p5 = p5;
+  }
+
+  upLevel() {
+    this.pos.y = this.pos.y - this.size;
+  }
+
+  show() {
+    this.p5.noStroke();
+    if (this.active) {
+      this.p5.fill(65);
+    } else {
+      this.p5.fill(this.color);
+    }
+    this.p5.rect(this.pos.x, this.pos.y, this.size, this.size);
+  }
+}
+
+function createNewAr(prevRow: Cell[], idx: number, p5: p5Types) {
+  const newRow = [];
+  for (let i = 0; i < prevRow.length; i++) {
+    newRow.push(
+      new Cell(i * size, idx * size, size, applyRules(prevRow, i), p5)
+    );
+  }
+  return newRow;
+}
+
+function applyRules(row: Cell[], idx: number) {
+  const keys = Object.keys(rules);
+  for (const key of keys) {
+    const rule = key.split(" ");
+    const firstCell = row[idx - 1] ? row[idx - 1].active : false;
+    const middleCell = row[idx].active;
+    const lastCell = row[idx + 1] ? row[idx + 1].active : false;
+    if (
+      firstCell === getBoolFromS(rule[0]) &&
+      middleCell === getBoolFromS(rule[1]) &&
+      lastCell === getBoolFromS(rule[2])
+    ) {
+      return rules[key];
+    }
+  }
+  return false;
+}
+
+function getBoolFromS(value: string) {
+  return value === "0" ? false : true;
+}
--- src/components/ElementaryCA/Home.tsx
+++ src/components/ElementaryCA/Home.tsx
@@ -1,17 +1,17 @@
-import { Fragment } from "react";
 import { Rules } from "./Rules";
 import { Button } from "../UI/Button";
 
 export const Home = () => (
-  <Fragment>
+  <div>
     <div className="elementaryFront">
       <p className="elementaryTitle">Ken's Cellular Automata 1D</p>
       <p className="elementarySubtitle">Define a rule for each thing</p>
     </div>
+    {/* <Canvas /> */}
     <Rules />
     <p className="elementaryRuleNumber">Rule 30</p>
     <div className="elementaryStart">
       <Button type="start" action={() => {}} />
     </div>
-  </Fragment>
+  </div>
 );
